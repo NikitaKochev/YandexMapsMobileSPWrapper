3a3ae5d69a5f5a61fdd9509809442ad7eda7feb3
NSKochev <nikitakochev.iosdev@gmail.com>
2023-11-29 11:22:27 +0300

    [Package] Added target and dependencies

--- Package.swift
+++ Package.swift
@@ -5,19 +5,40 @@ import PackageDescription
 
 let package = Package(
     name: "YandexMapsMobileSPWrapper",
+    platforms: [
+        .iOS(.v12),
+    ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
+        // Products define the executables and libraries a package produces, and
+        // make them visible to other packages.
         .library(
             name: "YandexMapsMobileSPWrapper",
-            targets: ["YandexMapsMobileSPWrapper"]),
+            targets: ["YandexMapsMobileSPWrapper"]
+        ),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        .package(
+            url: "https://github.com/NikitaKochev/YandexMapsMobileSP",
+            .exact("4.4.0")
+        ),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "YandexMapsMobileSPWrapper"),
-        .testTarget(
-            name: "YandexMapsMobileSPWrapperTests",
-            dependencies: ["YandexMapsMobileSPWrapper"]),
+        // Targets are the basic building blocks of a package. A target can define a
+        // module or a test suite.
+        // Targets can depend on other targets in this package, and on products in
+        // packages this package depends on.
+        .target(name: "YandexMapsMobileSPWrapper",
+                dependencies: [
+                    .product(name: "YandexMapsMobileSP", package: "YandexMapsMobileSP"),
+                ],
+                linkerSettings: [
+                    .linkedFramework("CoreLocation"),
+                    .linkedFramework("CoreTelephony"),
+                    .linkedFramework("SystemConfiguration"),
+                    .linkedFramework("CoreMotion"),
+                    .linkedLibrary("c++"),
+                    .unsafeFlags(["-ObjC"]),
+                ])
     ]
 )
